65c5cad1a12d941822f4f54d90e08b9977401bf4
thuhangnguyen8894 <thuhang.nguyen8894@gmail.com>
2017-03-07 09:11:52 +0700

    HangNguyen modified src/app/utils/jsonHelper/common/jsonCommon_smart_device_status_value/test/JsonCommon_smart_device_status_value_Test.cpp

--- src/app/utils/jsonHelper/common/jsonCommon_smart_device_status_value/test/JsonCommon_smart_device_status_value_Test.cpp
+++ src/app/utils/jsonHelper/common/jsonCommon_smart_device_status_value/test/JsonCommon_smart_device_status_value_Test.cpp
@@ -13,12 +13,14 @@
  * 2017-Jan-07 Modified tn-trang.tran@outlook.com
  * 2017-Jan-11 Modified tn-trang.tran@outlook.com
  * 2017-Feb-11 Modified tn-trang.tran@outlook.com
+ * 2017-Mar-06 Modified thuhang.nguyen8894@gmail.com
  */
 /****************************************************************************/
 
 #include <gtest/gtest.h>
 
 #include "JsonCommon_smart_device_status_value.h"
+#include <boost/property_tree/ptree.hpp>
 
 class JsonCommon_smart_device_status_value_Test : public ::testing::Test
 {
@@ -110,3 +112,84 @@ TEST_F(JsonCommon_smart_device_status_value_Test, TestgetJSONMessageType_DEFAULT
     MESSAGE_TYPE messageType = getJSONMessageType(message);
     EXPECT_EQ(messageType, MESSAGE_TYPE_DEFAULT);
 }
+
+/*!
+ * Test Type      : Equivalence Class Testing
+ * Expected Result[0]: 192.168.1.100
+ * Expected Result[1]: 8800
+ */
+TEST_F(JsonCommon_smart_device_status_value_Test, TestsplitWordRegex_RESULT_OK)
+{
+    std::string message("192.168.1.100:8800");
+    std::vector<std::string> status = splitWordRegex(message, COLON_SPLITTER);
+    EXPECT_EQ(status[0], "192.168.1.100");
+    EXPECT_EQ(status[1], "8800");
+}
+
+/*!
+ * Test Type      : Lower Boundary Testing
+ * Expected Result: false
+ */
+TEST_F(JsonCommon_smart_device_status_value_Test, TestisSensorMessage_FAILURE_COLON_SPLITTER)
+{
+    std::string message("SMART_DEVICE_STATUS_VALUE;SD001.OFF.UNACTIVE.R0001;192.168.0.100/8800;TI00000001_1/18/2017_21/59/00");
+    bool status = isSensorMessage(message);
+    EXPECT_FALSE(status);
+}
+
+/*!
+ * Test Type      : Equivalence Class Testing
+ * Expected Result: false
+ */
+TEST_F(JsonCommon_smart_device_status_value_Test, TestisSensorMessage_FAILURE_SLASH_SPLITTER)
+{
+    std::string message("SMART_DEVICE_STATUS_VALUE;SD001:OFF:UNACTIVE:R0001;192.168.0.100:8800;TI00000001_1:18:2017_21:59:00");
+    bool status = isSensorMessage(message);
+    EXPECT_FALSE(status);
+}
+
+/*!
+ * Test Type      : Equivalence Class Testing
+ * Expected Result: false
+ */
+TEST_F(JsonCommon_smart_device_status_value_Test, TestisSensorMessage_FAILURE_UNDERSCORS_SPLITTER)
+{
+    std::string message("SMARTDEVICE;SD001:OFF:UNACTIVE:R0001;192.168.0.100:8800;TI00000001:1/18/2017:21:59:00");
+    bool status = isSensorMessage(message);
+    EXPECT_FALSE(status);
+}
+
+/*!
+ * Test Type      : Equivalence Class Testing
+ * Expected Result: SMART_DEVICE_STATUS_VALUE
+ */
+TEST_F(JsonCommon_smart_device_status_value_Test, TestconvertMessageTypeToStr_RESULT_OK)
+{
+    MESSAGE_TYPE message = MESSAGE_TYPE_SMART_DEVICE_STATUS;
+    std::string value = convertMessageTypeToStr(message);
+    EXPECT_EQ(value, MESSAGE_TYPE_SMART_DEVICE_STATUS_VALUE);
+}
+
+/*!
+ * Test Type      : Equivalence Class Testing
+ * Expected Result: MESSAGE_TYPE_DEFAULT
+ */
+TEST_F(JsonCommon_smart_device_status_value_Test, TestconvertMessageTypeToStr_RESULT_FAILURE)
+{
+    MESSAGE_TYPE message = MESSAGE_TYPE_DEFAULT;
+    std::string value = convertMessageTypeToStr(message);
+    EXPECT_EQ(value, MESSAGE_TYPE_DEFAULT_VALUE);
+}
+
+/*!
+ * Test Type      : Equivalence Class Testing
+ * Expected Result: true
+ */
+
+/*TEST_F(JsonCommon_smart_device_status_value_Test, TestconvertJsonStrToPtree_RESULT_OK)
+{
+    boost::property_tree::ptree pTree;
+    std::string message("MESSAGE_TYPE : SMART_DEVICE_STATUS_VALUE");
+    bool status = convertJsonStrToPtree(message, pTree);
+    EXPECT_TRUE(status);
+}*/
