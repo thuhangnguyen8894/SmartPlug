245818c79bd49ba9461e5a05d3dda4f66c85b792
TrangTran <tn-trang.tran@outlook.com>
2017-05-24 13:55:11 +0700

    TrangTran MODIFIED src/app/processor/message_handler.py

--- src/app/processor/message_handler.py
+++ src/app/processor/message_handler.py
@@ -15,7 +15,8 @@ class MessageHandler(threading.Thread):
 
     def process_smart_device_status(self):
         try:
-            smart_device_info = self.json_parser.parse_smart_devices_status(self.message)
+            smart_device_info = self.json_parser.parse_smart_devices_status_info(\
+                                                                self.message)
             print("smart_device_info:", smart_device_info)
             self.lib_db_manager.insert_time(smart_device_info)
         except exceptions.InsertingTableDeviceTimerForCFailure as ex:
@@ -27,11 +28,44 @@ class MessageHandler(threading.Thread):
             traceback.print_exc()
             print("Error message 02: ", ex)
 
+    """[select data from table SmartDevice]
+    [the 18th of May]
+    """
+    def process_select_table_smart_device(self):
+        try:
+            list_devices = self.lib_db_manager.select_smartdevice()
+            emplements = len(list_devices)
+
+            for x in range(emplements):
+                print("Smart Device[", x, "]: ", 
+                                 list_devices[x].device_item['idSmartDevice'])
+                print(list_devices[x].device_item['idSmartDevice'])
+                print(list_devices[x].device_item['nameSmartDevice'])
+                print(list_devices[x].device_item['idRoom'])
+        except exceptions.SelectTableSmartDeviceForCFailure as ex:
+            import traceback
+            traceback.print_exc()
+            print("Error message 01: ", ex)
+        except Exception as ex:
+            import traceback
+            traceback.print_exc()
+            print("Error message 02: ", ex)
+
     def process_message(self):
         print("self.topic: ", self.topic)
         print("ATTR_SMART_DEVICE_STATUS_VALUE: ", constants.ATTR_SMART_DEVICE_STATUS_VALUE)
+
+        """[select data from table SmartDevice]
+        [the 18th of May]
+        """
+        if self.topic == "SELECTED_SMART_DEVICE":
+            self.process_select_table_smart_device()
+
+
         if self.topic == constants.ATTR_SMART_DEVICE_STATUS_VALUE:
             self.process_smart_device_status()
+        if self.topic == constants.ATTR_ANDROID_CONTROL_STATUS_DEVICE:
+            self.process_android_control_status_device()
 
     def run(self):
         self.lib_db_manager = lib_db_manager.LibDBManager()
