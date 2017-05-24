cf6c11435cab35cfd461a74c1285facc32aa5b24
TrangTran <tn-trang.tran@outlook.com>
2017-05-24 13:45:41 +0700

    TrangTran MODIFIED src/app/dbManager/cWrapper/DBSmartDeviceForC.cpp

--- src/app/dbManager/cWrapper/DBSmartDeviceForC.cpp
+++ src/app/dbManager/cWrapper/DBSmartDeviceForC.cpp
@@ -12,24 +12,52 @@ static DBSmartDevice db;
 
 bool insertToTableTimerForC(const SmartDeviceInfo* device)
 {
-	sql::Connection* con;
-
-	bool status = db.insertToTableTimer(con, *device);
+	bool status = db.insertToTableTimer(*device);
 	return status;
 }
 
 bool insertToTableDeviceTimerForC(const SmartDeviceInfo* device)
 {
-	sql::Connection* con;
-
-	bool status = db.insertToTableDeviceTimer(con, *device);
+	bool status = db.insertToTableDeviceTimer(*device);
 	return status;
 }
 
 bool selectIdTimerToTableTimerForC(SmartDeviceInfo* device)
 {
-	sql::Connection* con;
-
-	bool status = db.selectIdTimerToTableTimer(con, *device);
+	bool status = db.selectIdTimerToTableTimer(*device);
 	return status;
+}
+
+bool selectDeviceToTableSmartDeviceForC(SmartDeviceInfo** device, 
+														 int *numberOfElement)
+{
+	if (*device != NULL)
+    {
+        delete *device;
+        *device = NULL;
+    }
+
+    int index = 0;
+
+    SmartDeviceInfo* listSmartDeviceInfoTmp = new SmartDeviceInfo[2];
+    std::vector<SmartDeviceInfo> vectorSmartDeviceInfos;
+    bool result = db.selectDeviceToTableSmartDevice(vectorSmartDeviceInfos);
+
+    if (!result)
+    {
+        return false;
+    }
+
+	*numberOfElement = vectorSmartDeviceInfos.size();
+    for (index = 0; index < *numberOfElement; index++)
+    {
+        strcpy((listSmartDeviceInfoTmp[index]).device.idSmartDevice,
+                          vectorSmartDeviceInfos[index].device.idSmartDevice);
+        strcpy((listSmartDeviceInfoTmp[index]).device.nameSmartDevice,
+                          vectorSmartDeviceInfos[index].device.idSmartDevice);
+    }
+   
+    *device = listSmartDeviceInfoTmp;
+
+    return true;
 }
