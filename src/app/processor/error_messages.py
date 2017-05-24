efd1a1752159391af8ce740d9aa9854bd4faf1be
TrangTran <tn-trang.tran@outlook.com>
2017-05-24 13:50:09 +0700

    TrangTran MODIFIED src/app/processor/error_messages.py

--- src/app/processor/error_messages.py
+++ src/app/processor/error_messages.py
@@ -1,2 +1,3 @@
 ERROR_INSERT_INTO_TABLE_TIMER = "Cannot insert into Timer table"
 ERROR_INSERT_INTO_TABLE_DEVICE_TIMER = "Cannot insert into Deivce Timer table."
+ERROR_SELECT_FROM_TABLE_SMART_DEVICE = "Cannot select into SmartDevice table."
