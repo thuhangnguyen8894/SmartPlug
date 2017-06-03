74b2893a4f56fcf4cc02ce48a638086404fefa57
TrangTran <tn-trang.tran@outlook.com>
2017-06-03 14:30:01 +0700

    TrangTran MODIFIED src/app/processor/lib_db_manager.py

--- src/app/processor/lib_db_manager.py
+++ src/app/processor/lib_db_manager.py
@@ -11,31 +11,30 @@ from cffi_interfaces.__cffi_messageSender import messageSender_c
 import error_messages
 import exceptions
 
-from ctypes import *
-
 class LibDBManager(object):
     def __init__(self):
         pass
 
     def message_sender(self, id_device, status_device):
-        print("Bao Khanh is here")
         if id_device == "SD001":
-            print("id_device " + id_device)
             str1 = '192.168.0.106'
             host = messageSender_cffi.new("char[]", bytes(str1, "utf-8"))
-            print(host[0])
+            messageStr = self.fomart_messageStr(status_device)
         elif id_device == "SD002":
-            print("id_device " + id_device)
             host = messageSender_cffi.new("char[]", bytes('192.168.0.103',"utf-8"))
-            
-        messageStr = messageSender_cffi.new("char[]", bytes(status_device, "utf-8"))
-        print("status_devices: " + status_device)
+            messageStr = self.fomart_messageStr(status_device)
         
         port = 5600
 
         messageSender_c.sendMessageUDPForC(messageStr, host, port)
         print("sendMessageUDPForC")
-        
+
+    def fomart_messageStr(self, status_device):
+        if status_device == "ACTIVE":
+            messageStr = messageSender_cffi.new("char[]", bytes('1', "utf-8"))
+        elif status_device == "UNACTIVE":
+            messageStr = messageSender_cffi.new("char[]", bytes('0', "utf-8"))
+        return messageStr
 
     def insert_time(self, smart_device_info_dict):
         timing_data_ptr = db_mgr_cffi.new("SmartDeviceInfo* ",\
