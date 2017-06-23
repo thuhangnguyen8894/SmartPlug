7942b75a6ba65a0ace7abe93d6387b1128058614
thuhangnguyen8894 <thuhang.nguyen8894@gmail.com>
2017-06-23 18:50:37 +0700

    HangNguyen create Config.java

---
+++ android/SmartPlug/app/src/main/java/com/nguyenthuhang/smartplug/Constants.java
@@ -0,0 +1,8 @@
+package com.nguyenthuhang.smartplug;
+
+/**
+ * Created by hang on 21/06/2017.
+ */
+
+public class Constants {
+}
---
+++ android/SmartPlug/app/src/main/java/com/nguyenthuhang/smartplug/model/User.java
@@ -0,0 +1,8 @@
+package com.nguyenthuhang.smartplug.model;
+
+/**
+ * Created by hang on 20/06/2017.
+ */
+
+public class User {
+}
