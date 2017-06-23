4ae292a1ade9d9d81efe8e309036262021f71b1f
thuhangnguyen8894 <thuhang.nguyen8894@gmail.com>
2017-06-23 18:55:29 +0700

    HangNguyen modified views.py

--- wbArduino/arduino/views.py
+++ wbArduino/arduino/views.py
@@ -3,6 +3,7 @@ import socket
 import pickle
 import MySQLdb
 import string
+import datetime
 
 from arduino import constants
 
@@ -16,9 +17,9 @@ def arduino(request):
     return render(request, "arduino.html", {})
 
 def selectDevice(cmd):
-    print("Hang 04")
+    print("Hang 01")
     if cmd == 'SELECT_DEVICE':
-        print("Hang 05")
+        # print("Hang 02")
         conn = MySQLdb.connect(host = constants.HOST, user = constants.USER, passwd = constants.PASSWORD, db = constants.DATABASE)
         cur = conn.cursor()
         cur.execute("SELECT idSmartDevice, nameSmartDevice, stateElectric, stateRelay FROM SmartDevice;")
@@ -37,38 +38,124 @@ def selectDevice(cmd):
         conn.close()
         return json_array
 
+def loginUser(cmd, json_user_name, json_password):
+    if cmd == 'LOGIN_DEVICE':
+        conn = MySQLdb.connect(host = constants.HOST, user = constants.USER, passwd = constants.PASSWORD, db = constants.DATABASE)
+        cur = conn.cursor()
+        cur.execute("SELECT userName, password, stateUser FROM User")
+        json_dict = {}
+        for r in cur:
+            if r[0] == json_user_name and r[1] == json_password:
+                if r[2] == 'ACTIVE':
+                    print("json_user_name success ", json_user_name)
+                    print("json_password success", json_password)
+                    print("r[0] success ", r[0])
+                    print("r[1] success", r[1])
+                    json_dict = { constants.ATTR_JSON_MESSAGE_TYPE: constants.ATTR_JSON_LOGIN_SUCCESSFUL}
+                    # return json_dict
+                elif r[2] == 'UNACTIVE':
+                    json_dict = { constants.ATTR_JSON_MESSAGE_TYPE: constants.ATTR_JSON_LOGIN_ERROR_UNACTIVE}
+                print("USER ACTIVE UNACTIVE", json_dict)
+                return json_dict
+            else:
+                print("json_user_name error", json_user_name)
+                print("json_password error", json_password)
+                print("r[0] error ", r[0])
+                print("r[1] error", r[1])
+                json_dict = { constants.ATTR_JSON_MESSAGE_TYPE: constants.ATTR_JSON_LOGIN_ERROR}
+        cur.close()
+        conn.close()
+        return json_dict
+
+def existUsername(json_user_name):
+    conn = MySQLdb.connect(host = constants.HOST, user = constants.USER, passwd = constants.PASSWORD, db = constants.DATABASE)
+    cur = conn.cursor()
+    cur.execute("SELECT userName FROM User")
+    for r in cur:
+        if r[0] == json_user_name:
+            return True
+    cur.close()
+    conn.close()
+    return False
+
+def existEmail(json_email):
+    conn = MySQLdb.connect(host = constants.HOST, user = constants.USER, passwd = constants.PASSWORD, db = constants.DATABASE)
+    cur = conn.cursor()
+    cur.execute("SELECT email FROM User")
+    for r in cur:
+        if r[0] == json_email:
+            return True
+    cur.close()
+    conn.close()
+    return False
+
+def autoIDUser():
+    iduser = 'US'
+
+    date_time = datetime.datetime.now()
+
+    year_auto = str(date_time.year)
+
+    month_auto = str(date_time.month)
+    if len(month_auto) == 1:
+        month_auto = '0' + month_auto
+
+    day_auto = str(date_time.day)
+    if len(day_auto) == 1:
+        day_auto = '0' + day_auto
+
+    hour_auto = str(date_time.hour)
+    if len(hour_auto) == 1:
+        hour_auto = '0' + hour_auto
+
+    minute_auto = str(date_time.minute)
+    if len(minute_auto) == 1:
+        minute_auto = '0' + minute_auto
+
+    second_auto = str(date_time.second)
+    if len(second_auto) == 1:
+        second_auto = '0' + second_auto
+
+    iduser = iduser + year_auto + month_auto + day_auto + hour_auto + minute_auto + second_auto
+    print("ID USER 01: " , iduser)
+
+    return iduser
+
+def registerUser(json_user_name, json_password, json_email):
+    json_dict_register = {}
+    b_username = existUsername(json_user_name)
+    b_email = existEmail(json_email)
+    print("b_username: ", b_username)
+    print("b_email: ", b_email)
+
+    if b_username == False: 
+        if b_email == False:
+            iduser = autoIDUser()
+            print("ID USER 02: " + iduser)
+            conn = MySQLdb.connect(host = constants.HOST, user = constants.USER, passwd = constants.PASSWORD, db = constants.DATABASE)
+            cur = conn.cursor()
+            print("db 02")
+            try: 
+                cur.execute("INSERT INTO User VALUES (%s,%s,%s,%s,%s)", (iduser, json_user_name, json_password, json_email, 'UNACTIVE'))
+                print("db 03")
+                conn.commit()
+                json_dict_register = {constants.ATTR_JSON_MESSAGE_TYPE: constants.ATTR_JSON_REGISTER_SUCCESSFUL}
+                print("Register Successful: " , json_dict_register)
+            except:
+                conn.rollback()
+                json_dict_register = {constants.ATTR_JSON_MESSAGE_TYPE: constants.ATTR_JSON_REGISTER_ERROR}
+                print("Register Successful: " , json_dict_register)
+            conn.close()
+        else:
+            json_dict_register = {constants.ATTR_JSON_MESSAGE_TYPE: constants.ATTR_JSON_REGISTER_EMAIL_EXIST}
+            print("Email exist " , json_dict_register)
+    else:
+        json_dict_register = {constants.ATTR_JSON_MESSAGE_TYPE: constants.ATTR_JSON_REGISTER_USERNAME_EXIST}
+        print("Username exist" , json_dict_register)  
+    return json_dict_register      
+
+
 def controlSmartPlug(cmd,json_id_device,json_name_device):
-    # # cmd = request.GET['cmd']
-    # print("CMD2: ", cmd)
-    # if cmd == '1':
-    #     json_dict_on = { 
-    #         constants.ATTR_JSON_MESSAGE_TYPE : constants.ATTR_JSON_MOBILE_STATUS_VALUE, \
-    #         constants.ATTR_JSON_DATA : { \
-    #             constants.ATTR_JSON_ID_DEVICE : json_id_device,\
-    #             constants.ATTR_JSON_NAME_DEVICE : "DEVICE_LIGHT",\
-    #             constants.ATTR_JSON_RELAY_STATUS_VALUE : constants.ATTR_JSON_RELAY_STATUS_VALUE_ACTIVE,\
-    #             constants.ATTR_JSON_ID_ROOM : "R001"
-    #         }
-    #     }
-    #     print("CMD ON: ", json_dict_on) 
-    #     sock.sendto(json.dumps(json_dict_on).encode(), (MESSAGE_RECEIVER_IP, MESSAGE_RECEIVER_PORT))
-        
-    # elif cmd == '0':
-    #     json_dict_off = { 
-    #         constants.ATTR_JSON_MESSAGE_TYPE : constants.ATTR_JSON_MOBILE_STATUS_VALUE, \
-    #         constants.ATTR_JSON_DATA : { \
-    #             constants.ATTR_JSON_ID_DEVICE : json_id_device,\
-    #             constants.ATTR_JSON_NAME_DEVICE : "DEVICE_LIGHT",\
-    #             constants.ATTR_JSON_RELAY_STATUS_VALUE : constants.ATTR_JSON_RELAY_STATUS_VALUE_UNACTIVE,\
-    #             constants.ATTR_JSON_ID_ROOM : "R001"
-    #         }
-    #     } 
-    #     print("CMD OFF: ", json_dict_off)
-    #     sock.sendto(json.dumps(json_dict_off).encode(), (MESSAGE_RECEIVER_IP, MESSAGE_RECEIVER_PORT))
-    # return JsonResponse({'SPState ':cmd})
-
-    # cmd = request.GET['cmd']
-
     json_dict_message = {}
     json_message_data = None
     json_dict_message[constants.ATTR_JSON_MESSAGE_TYPE] = str(constants.ATTR_JSON_MOBILE_STATUS_VALUE)
@@ -89,7 +176,7 @@ def controlSmartPlug(cmd,json_id_device,json_name_device):
 
     print("json_dict_message" , json_dict_message)
     sock.sendto(json.dumps(json_dict_message).encode('utf-8'), (constants.MESSAGE_RECEIVER_IP,\
-                                                        constants.MESSAGE_RECEIVER_PORT))
+                                                        constants.MESSAGE_RECEIVER_PORT))    
 
 
 def serverDjango(request):
@@ -102,12 +189,12 @@ def serverDjango(request):
     json_mes = str(json_parse['MESSAGE_STATUS_VALUE'])
 
     if json_mes == 'SELECT_DEVICE':
-        print("Hang 03:", json_mes)
+        print("SELECT_DEVICE:", json_mes)
         json_dict = selectDevice(json_mes)
         return JsonResponse({ 'JSON_SEL' : json_dict })
 
     elif json_mes == 'ON_OFF_DEVICE':
-        print("Hang 04" , json_mes)
+        print("ON_OFF_DEVICE " , json_mes)
         json_id_device = str(json_parse['ID_DEVICE'])
         json_name_device = str(json_parse['NAME_DEVICE'])
         json_status_device = str(json_parse['ELECTRIC_STATUS_VALUE'])
@@ -119,8 +206,27 @@ def serverDjango(request):
         controlSmartPlug(json_status_device,json_id_device,json_name_device)
         return JsonResponse({'SPState ': json_status_device})
 
+    elif json_mes =='LOGIN_DEVICE':
+        print("Login: " , json_mes)
 
+        json_user_name = str(json_parse['USER_NAME'])
+        json_password = str(json_parse['PASSWORD'])
 
- 
+        json_dict_login = loginUser(json_mes, json_user_name, json_password)
+        print("json_dict_login: " , json_dict_login)
 
+        return JsonResponse(json_dict_login)
 
+    elif json_mes == 'REGISTER_DEVICE':
+        print("Register: " , json_mes)
+
+        json_user_name = str(json_parse['USER_NAME'])
+        print("1: " + json_user_name)
+        json_password = str(json_parse['PASSWORD'])
+        print("2: " + json_password)
+        json_email = str(json_parse['EMAIL'])
+        print("3: " + json_email)
+
+        json_dict_register = registerUser(json_user_name, json_password, json_email)
+        print("4: "  , json_dict_register)
+        return JsonResponse(json_dict_register)
